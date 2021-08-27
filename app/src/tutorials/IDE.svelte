745edc129467eb956c7ee061d7d3aa290b8e90f2
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-08-27 14:54:59 -0700

    Checkpoint

---
+++ app/src/tutorials/IDE.svelte
@@ -0,0 +1,41 @@
+<script>
+import { onMount } from "svelte";
+
+// 2.2MB
+
+// https://github.com/fudgepop01/svelte-monaco-editor-example/blob/master/src/components/monaco/monaco.js
+import 'monaco-editor/esm/vs/basic-languages/python/python.contribution.js';
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js';
+
+let containerElt;
+onMount(() => {
+	const editor = monaco.editor.create(containerElt, {
+		value: `def test():\n\tprint(123)\n`,
+		theme: "vs-light",
+		language: "python",
+		minimap: { enabled: false }
+    });
+
+	editor.addAction({
+		id: 'my-unique-id',
+		label: 'My Label!!!',
+		keybindings: [
+			monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter,
+		],
+		// A precondition for this action.
+		precondition: null,
+		// A rule to evaluate on top of the precondition in order to dispatch the keybindings.
+		keybindingContext: null,
+		contextMenuGroupId: 'navigation',
+		contextMenuOrder: 1.5,
+		// Method that will be executed when the action is triggered.
+		// @param editor The editor instance is passed in as a convinience
+		run: function(ed) {
+			alert("i'm running => " + ed.getPosition());
+			return null;
+		}
+	});
+});
+</script>
+
+<div bind:this={containerElt} id="container-editor" style="height: 50%"></div>
